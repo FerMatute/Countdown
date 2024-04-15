33ebe5ba9237010567e83f9511ae10713dbcaba1
Fernando Matute <105733447+FerMatute@users.noreply.github.com>
2024-04-15 14:38:45 -0600

    Date updated

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Aug 3, 2024',
+  birthdate: 'Jun 18, 2024',
   name: 'Chiquiiiiiiii'
 };
 
